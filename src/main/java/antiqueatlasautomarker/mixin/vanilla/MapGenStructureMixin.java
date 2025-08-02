97bcd685395ef8b9e123b5713adac6a9324874b6
Nischhelm <nischhelm@gmail.com>
2025-08-02 11:06:36 +0200

    fixed vanilla structs not actually marking

--- src/main/java/antiqueatlasautomarker/mixin/vanilla/MapGenStructureMixin.java
+++ src/main/java/antiqueatlasautomarker/mixin/vanilla/MapGenStructureMixin.java
@@ -2,8 +2,6 @@ package antiqueatlasautomarker.mixin.vanilla;
 
 import antiqueatlasautomarker.config.AutoMarkSetting;
 import antiqueatlasautomarker.structuremarkers.StructureMarkersDataHandler;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.structure.MapGenStructure;
@@ -12,17 +10,20 @@ import net.minecraft.world.gen.structure.StructureBoundingBox;
 import net.minecraft.world.gen.structure.StructureStart;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MapGenStructure.class)
 public class MapGenStructureMixin {
-    @WrapOperation(
+    @Inject(
             method = "generateStructure",
             at = @At(value = "NEW", target = "(IIII)Lnet/minecraft/world/gen/structure/StructureBoundingBox;")
     )
-    private StructureBoundingBox aaam_markStructures(int xMin, int zMin, int xMax, int zMax, Operation<StructureBoundingBox> original, @Local StructureStart structureStart, @Local(argsOnly = true) World worldIn){
+    private void aaam_markStructures(CallbackInfoReturnable<Boolean> cir, @Local StructureStart structureStart, @Local(argsOnly = true) World worldIn){
         AutoMarkSetting setting = AutoMarkSetting.get(MapGenStructureIO.getStructureStartName(structureStart));
-        if(setting != null && setting.enabled)
-            StructureMarkersDataHandler.markStructure(worldIn, structureStart.getChunkPosX() << 4 + 8, structureStart.getChunkPosZ() << 4 + 8, setting);
-        return original.call(xMin, zMin, xMax, zMax);
+        if(setting != null && setting.enabled) {
+            StructureBoundingBox box = structureStart.getBoundingBox();
+            StructureMarkersDataHandler.markStructure(worldIn, (box.maxX + box.minX)/2, (box.maxZ + box.minZ) / 2, setting);
+        }
     }
 }

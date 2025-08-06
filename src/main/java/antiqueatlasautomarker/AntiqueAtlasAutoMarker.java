21fd19ad2261e533d08c6675482b437ea4e8914a
Nischhelm <nischhelm@gmail.com>
2025-08-06 18:04:04 +0200

    bump v1.3.2
    v1.3.2
    - fixed vanilla structure markers actually not marking correctly (bruh)
    - allowed marker type selection to scroll vertically (much wow)
    - updated to fermiumbooter 1.3.2 - disabling markers now fully disables the mixins (more compat)
    
    v1.3.1
    - fixed waystone markers not showing in tp select screen if hidden in normal view
    - added config toggles for hiding marker types and select waystone on map button
    - fixed firereceivedstructuremarkerevent toggle AGAIN
    - stopped putmarker command giving clients current coords

--- src/main/java/antiqueatlasautomarker/AntiqueAtlasAutoMarker.java
+++ src/main/java/antiqueatlasautomarker/AntiqueAtlasAutoMarker.java
@@ -30,7 +30,7 @@ import org.apache.logging.log4j.Logger;
 )
 public class AntiqueAtlasAutoMarker {
     public static final String MODID = "antiqueatlasautomarker";
-    public static final String VERSION = "1.3.1"; 
+    public static final String VERSION = "1.3.2";
     public static final String NAME = "AntiqueAtlasAutoMarker";
     public static final Logger LOGGER = LogManager.getLogger();
     public static final boolean isDebugging = false;

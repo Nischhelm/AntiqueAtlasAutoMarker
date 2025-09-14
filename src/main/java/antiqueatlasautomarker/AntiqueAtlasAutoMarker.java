66b8fe89714bf4bc84792facba477e0cd22e972f
Nischhelm <nischhelm@gmail.com>
2025-09-14 21:12:42 +0200

    v1.4.0
    - colored biome tiles galore
    - can change village tiles to custom
    - show nether and end custom biome tiles correctly with void/lava/nether inwall
    - compat for OTG villages
    - allowed changing the rules how AA selects tiles for custom biomes automatically by biome type
    - fixed structure watchers being pretty bad for performance
    - fixed mixinconfigs crashing sometimes (sorry, you need to go over the "enabled" configs again as they are renamed now)

--- src/main/java/antiqueatlasautomarker/AntiqueAtlasAutoMarker.java
+++ src/main/java/antiqueatlasautomarker/AntiqueAtlasAutoMarker.java
@@ -32,7 +32,7 @@ import org.apache.logging.log4j.Logger;
 )
 public class AntiqueAtlasAutoMarker {
     public static final String MODID = "antiqueatlasautomarker";
-    public static final String VERSION = "1.3.2";
+    public static final String VERSION = "1.4.0";
     public static final String NAME = "AntiqueAtlasAutoMarker";
     public static final Logger LOGGER = LogManager.getLogger();
     public static final boolean isDebugging = false;

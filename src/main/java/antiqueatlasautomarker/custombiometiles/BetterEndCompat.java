c11bd58649df5bb971c5119605e557a503804983
Nischhelm <nischhelm@gmail.com>
2025-09-16 22:40:43 +0200

    added code for the new betterend tiles

--- src/main/java/antiqueatlasautomarker/custombiometiles/BetterEndCompat.java
+++ src/main/java/antiqueatlasautomarker/custombiometiles/BetterEndCompat.java
@@ -4,10 +4,14 @@ import hunternif.mc.atlas.AntiqueAtlasMod;
 import hunternif.mc.atlas.api.AtlasAPI;
 import hunternif.mc.atlas.api.TileAPI;
 import hunternif.mc.atlas.client.TextureSet;
+import hunternif.mc.atlas.client.Textures;
+import hunternif.mc.atlas.ext.ExtTileIdMap;
 import mod.beethoven92.betterendforge.common.init.ModBiomes;
 import net.minecraft.util.ResourceLocation;
 
 public class BetterEndCompat {
+    public static int BE_ICE_STARFIELD_VOID;
+
     public static void registerTiles(){
         TileAPI api = AtlasAPI.getTileAPI();
 
@@ -18,7 +22,10 @@ public class BetterEndCompat {
         TextureSet glowingGrasslands = new TextureSet("BE_GLOWING_GRASSLANDS", tileLoc("glowing_grasslands"));
         TextureSet crystalMountains = new TextureSet("BE_CRYSTAL_MOUNTAINS", tileLoc("holy_crystal_mountains"), tileLoc("holy_crystal_mountains2"));
         TextureSet iceStarfield = new TextureSet("BE_ICE_STARFIELD", tileLoc("ice_starfield"), tileLoc("ice_starfield2"));
+        TextureSet iceStarfieldVoid = new TextureSet("BE_ICE_STARFIELD_VOID", tileLoc("ice_starfield_void2"), Textures.TILE_END_VOID).stitchToMutual(TextureSet.END_VOID);
         TextureSet megalakeGrove = new TextureSet("BE_MEGALAKE_GROVE", tileLoc("megalake_grove"), tileLoc("megalake_grove"), tileLoc("megalake_grove2"));
+        TextureSet neonOasis = new TextureSet("BE_NEON_OASIS", tileLoc("neon_oasis4"));
+        TextureSet paintedMountains = new TextureSet("BE_PAINTED_MOUNTAINS", tileLoc("painted_mountains"));
         TextureSet shadowForest = new TextureSet("BE_SHADOW_FOREST", tileLoc("shadowforest"), tileLoc("shadowforest"), tileLoc("shadowforest2"));
         TextureSet sulphurSprings = new TextureSet("BE_SULPHUR_SPRINGS", tileLoc("sulpher_springs"), tileLoc("sulpher_springs2"));
         TextureSet umbraValley = new TextureSet("BE_UMBRA_VALLEY", tileLoc("umbra_valley"), tileLoc("umbra_valley2"));
@@ -33,7 +40,10 @@ public class BetterEndCompat {
         api.setBiomeTexture(ModBiomes.GLOWING_GRASSLANDS.getBiome(), glowingGrasslands);
         api.setBiomeTexture(ModBiomes.CRYSTAL_MOUNTAINS.getBiome(), crystalMountains);
         api.setBiomeTexture(ModBiomes.ICE_STARFIELD.getBiome(), iceStarfield);
+        BE_ICE_STARFIELD_VOID = registerCustomVoidTexture(api, iceStarfieldVoid);
         api.setBiomeTexture(ModBiomes.MEGALAKE_GROVE.getBiome(), megalakeGrove);
+        api.setBiomeTexture(ModBiomes.NEON_OASIS.getBiome(), neonOasis);
+        api.setBiomeTexture(ModBiomes.PAINTED_MOUNTAINS.getBiome(), paintedMountains);
         api.setBiomeTexture(ModBiomes.SHADOW_FOREST.getBiome(), shadowForest);
         api.setBiomeTexture(ModBiomes.SULPHUR_SPRINGS.getBiome(), sulphurSprings);
         api.setBiomeTexture(ModBiomes.UMBRA_VALLEY.getBiome(), umbraValley);
@@ -41,11 +51,15 @@ public class BetterEndCompat {
 
         api.setBiomeTexture(ModBiomes.MEGALAKE.getBiome(), todoBiome);
         api.setBiomeTexture(ModBiomes.FOGGY_MUSHROOMLAND.getBiome(), todoBiome);
-        api.setBiomeTexture(ModBiomes.PAINTED_MOUNTAINS.getBiome(), todoBiome);
         api.setBiomeTexture(ModBiomes.BLOSSOMING_SPIRES.getBiome(), todoBiome);
         api.setBiomeTexture(ModBiomes.AMBER_LAND.getBiome(), todoBiome);
         api.setBiomeTexture(ModBiomes.LANTERN_WOODS.getBiome(), todoBiome);
-        api.setBiomeTexture(ModBiomes.NEON_OASIS.getBiome(), todoBiome);
+    }
+
+    private static int registerCustomVoidTexture(TileAPI api, TextureSet set){
+        api.registerTextureSet(set);
+        api.setCustomTileTexture(set.name, set);
+        return ExtTileIdMap.instance().getOrCreatePseudoBiomeID(set.name);
     }
 
     private static ResourceLocation tileLoc(String tileName){

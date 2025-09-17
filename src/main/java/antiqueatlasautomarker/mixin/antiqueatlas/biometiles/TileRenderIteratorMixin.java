b413f8801f54f3038ed1d5d7bb90e0b08233e32a
Nischhelm <nischhelm@gmail.com>
2025-09-17 22:16:40 +0200

    void stitches to null, fix stitch to null,

---
+++ src/main/java/antiqueatlasautomarker/mixin/antiqueatlas/biometiles/TileRenderIteratorMixin.java
@@ -0,0 +1,27 @@
+package antiqueatlasautomarker.mixin.antiqueatlas.biometiles;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import hunternif.mc.atlas.client.BiomeTextureMap;
+import hunternif.mc.atlas.client.TextureSet;
+import hunternif.mc.atlas.client.TileRenderIterator;
+import hunternif.mc.atlas.core.Tile;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Debug(export = true)
+@Mixin(TileRenderIterator.class)
+public abstract class TileRenderIteratorMixin {
+
+    @WrapOperation(
+            method = {"shouldStitchTo", "shouldStitchToHorizontally", "shouldStitchToVertically"},
+            at = @At(value = "INVOKE", target = "Lhunternif/mc/atlas/client/BiomeTextureMap;getTextureSet(Lhunternif/mc/atlas/core/Tile;)Lhunternif/mc/atlas/client/TextureSet;", ordinal = 1),
+            remap = false
+    )
+    private static TextureSet aaam_makeVoidStitchToNull(BiomeTextureMap instance, Tile tile, Operation<TextureSet> original, Tile tileFrom, Tile tileTo, @Local TextureSet setFrom){
+        //return tile == null ? null : original.call(instance, tile);
+        return original.call(instance, tile);
+    }
+}

67ce0069a3f3643c8902a5468f1e2f284d35b758
Nischhelm <nischhelm@gmail.com>
2025-09-23 17:14:41 +0200

    typo

--- src/main/java/antiqueatlasautomarker/custombiometiles/ThaumcraftCompat.java
+++ src/main/java/antiqueatlasautomarker/custombiometiles/ThaumcraftCompat.java
@@ -12,12 +12,12 @@ public class ThaumcraftCompat {
         TileAPI api = AtlasAPI.getTileAPI();
 
         TextureSet eerie = new TextureSet("TC_EERIE", tileLoc("tc_eeire"));
-        TextureSet magicalForesst = new TextureSet("TC_MAGICAL", tileLoc("tc_magicalforest1"), tileLoc("tc_magicalforest2"), tileLoc("tc_magicalforest3"), tileLoc("tc_magicalforest4"), tileLoc("tc_magicalforest5"));
+        TextureSet magicalForest = new TextureSet("TC_MAGICAL", tileLoc("tc_magicalforest1"), tileLoc("tc_magicalforest2"), tileLoc("tc_magicalforest3"), tileLoc("tc_magicalforest4"), tileLoc("tc_magicalforest5"));
         //TextureSet eldritch = new TextureSet("TC_ELDRITCH", tileLoc("tc_eldritch"));
         //TextureSet taint = new TextureSet("TC_TAINT", tileLoc("tc_taint1"), tileLoc("tc_taint2"), tileLoc("tc_taint3"), tileLoc("tc_taint4"), tileLoc("tc_taint5"));
 
         api.setBiomeTexture(BiomeHandler.EERIE, eerie);
-        api.setBiomeTexture(BiomeHandler.MAGICAL_FOREST, magicalForesst);
+        api.setBiomeTexture(BiomeHandler.MAGICAL_FOREST, magicalForest);
         //api.setBiomeTexture(BiomeHandler.ELDRITCH, eldritch);  //Golrith didn't make a TextureSet for this biome
         //api.setBiomeTexture(BiomeHandler.biomeTaint, taint);  //taint biome doesnt exist as a separate biome anymore since TC 6.0
     }

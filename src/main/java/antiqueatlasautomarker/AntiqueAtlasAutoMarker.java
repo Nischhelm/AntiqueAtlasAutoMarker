9267965b0d8e6bb44b7d22abab3c652afb22e6c2
Nischhelm <nischhelm@gmail.com>
2025-09-23 18:41:21 +0200

    v1.4.2
    - fixed custom position markers
    - fixed crash when disabling some betterend biomes
    - added betternether tiles (thanks artsy!)
    - fixed how nether biome tiles are assigned (more biome, less wall/lava)

--- src/main/java/antiqueatlasautomarker/AntiqueAtlasAutoMarker.java
+++ src/main/java/antiqueatlasautomarker/AntiqueAtlasAutoMarker.java
@@ -32,7 +32,7 @@ import org.apache.logging.log4j.Logger;
 )
 public class AntiqueAtlasAutoMarker {
     public static final String MODID = "antiqueatlasautomarker";
-    public static final String VERSION = "1.4.1.1";
+    public static final String VERSION = "1.4.2";
     public static final String NAME = "AntiqueAtlasAutoMarker";
     public static final Logger LOGGER = LogManager.getLogger();
     public static final boolean isDebugging = false;

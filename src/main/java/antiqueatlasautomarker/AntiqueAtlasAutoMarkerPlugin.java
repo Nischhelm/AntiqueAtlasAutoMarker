b1d4c6d246ca266b8e1b9a42bfda2615392578bb
nischhelm <nischhelm@gmail.com>
2026-04-01 01:01:09 +0200

    disable vanilla colorised tiles mixin if NONE

--- src/main/java/antiqueatlasautomarker/AntiqueAtlasAutoMarkerPlugin.java
+++ src/main/java/antiqueatlasautomarker/AntiqueAtlasAutoMarkerPlugin.java
@@ -22,8 +22,8 @@ public class AntiqueAtlasAutoMarkerPlugin implements IFMLLoadingPlugin {
 
 		//Antique Atlas Structure Markers
 		FermiumRegistryAPI.enqueueMixin(true, "mixins.aaam.antiqueatlas.structuremarkers.json");
-		FermiumRegistryAPI.enqueueMixin(true, "mixins.aaam.antiqueatlas.tiles.colorisedvanillatiles.json");
 
+		FermiumRegistryAPI.enqueueMixin(true, "mixins.aaam.antiqueatlas.tiles.colorisedvanillatiles.json", () -> !EarlyConfigReader.getString("Use Colorised Vanilla Tiles", ConfigHandler.overhaul.tileConfig.useColorisedVanillaTiles.toString()).equals("NONE"));
 		FermiumRegistryAPI.enqueueMixin(true, "mixins.aaam.antiqueatlas.overhaul.updateside.json", () -> !EarlyConfigReader.getString("Atlas Scanning Update Side", ConfigHandler.overhaul.updateSide.toString()).equals("DISABLE_MIXIN"));
 
 		FermiumRegistryAPI.enqueueMixin(true, "mixins.aaam.antiqueatlas.overhaul.structurewatchers.json"); //TODO add toggle

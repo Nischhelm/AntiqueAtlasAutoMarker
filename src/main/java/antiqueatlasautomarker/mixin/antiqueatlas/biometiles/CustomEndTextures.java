faa3c8c36c41c3b670d76b2ef361eeb923a52f5e
nischhelm <nischhelm@gmail.com>
2026-03-31 22:29:39 +0200

    fix end tile mixin
    crazy that the mixin.clinit hasnt ran yet during injection into target.clinit

--- src/main/java/antiqueatlasautomarker/mixin/antiqueatlas/biometiles/CustomEndTextures.java
+++ src/main/java/antiqueatlasautomarker/mixin/antiqueatlas/biometiles/CustomEndTextures.java
@@ -1,6 +1,5 @@
 package antiqueatlasautomarker.mixin.antiqueatlas.biometiles;
 
-import antiqueatlasautomarker.config.ConfigHandler;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import hunternif.mc.atlas.client.Textures;
@@ -8,29 +7,28 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.List;
 
 @Mixin(Textures.class)
 public abstract class CustomEndTextures {
+    @Unique private static List<String> aaam$endTextures = null;
+
     @ModifyExpressionValue(
             method = "tile",
             at = @At(value = "INVOKE", target = "Ljava/lang/StringBuilder;toString()Ljava/lang/String;"),
             remap = false
     )
     private static String aaam_swapToModifiedEnd(String original, @Local(argsOnly = true) String fileName){
-        if(!ConfigHandler.overhaul.tileConfig.useModifiedEndTiles) return original;
+        if(aaam$endTextures == null) aaam$endTextures = Arrays.asList( //cant statically clinit cause this runs in Textures.clinit... never had that situation before
+                "end_island.png",
+                "end_island2.png",
+                "end_island_plants.png",
+                "end_island_plants2.png",
+                "end_void.png"
+        );
         if(aaam$endTextures.contains(fileName))
             return "antiqueatlas:textures/gui/tiles/nischhelm/end/" + fileName;
         return original;
     }
-
-    @Unique private static final Set<String> aaam$endTextures = Stream.of(
-            "end_island",
-            "end_island2",
-            "end_island_plants",
-            "end_island_plants2",
-            "end_void"
-    ).collect(Collectors.toSet());
 }

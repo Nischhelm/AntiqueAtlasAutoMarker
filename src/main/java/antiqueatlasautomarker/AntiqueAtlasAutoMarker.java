d0af66a3b8f7f051b06f438ad06615b5c33d5b17
nischhelm <nischhelm@gmail.com>
2026-04-13 21:38:08 +0200

    v1.5.4.3
    allow to disable each added custom marker individually as suggested in #14

--- src/main/java/antiqueatlasautomarker/AntiqueAtlasAutoMarker.java
+++ src/main/java/antiqueatlasautomarker/AntiqueAtlasAutoMarker.java
@@ -40,7 +40,7 @@ import java.util.Map;
 )
 public class AntiqueAtlasAutoMarker {
     public static final String MODID = "antiqueatlasautomarker";
-    public static final String VERSION = "1.5.4.2";
+    public static final String VERSION = "1.5.4.3";
     public static final String NAME = "AntiqueAtlasAutoMarker";
     public static final Logger LOGGER = LogManager.getLogger();
     public static Configuration CONFIG;

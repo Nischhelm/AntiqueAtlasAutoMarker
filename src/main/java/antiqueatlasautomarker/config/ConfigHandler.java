c8627bbaa58dbe1bf8ff63347d00ab600e1b8271
nischhelm <nischhelm@gmail.com>
2026-09-24 09:20:13 +0200

    import optim after pr

--- src/main/java/antiqueatlasautomarker/config/ConfigHandler.java
+++ src/main/java/antiqueatlasautomarker/config/ConfigHandler.java
@@ -1,25 +1,7 @@
 package antiqueatlasautomarker.config;
 
 import antiqueatlasautomarker.AntiqueAtlasAutoMarker;
-import antiqueatlasautomarker.config.folders.AAOverhaulConfig;
-import antiqueatlasautomarker.config.folders.AARCAddonConfig;
-import antiqueatlasautomarker.config.folders.BattletowersConfig;
-import antiqueatlasautomarker.config.folders.BetterMineshaftConfig;
-import antiqueatlasautomarker.config.folders.CustomPositionConfig;
-import antiqueatlasautomarker.config.folders.DefiledLandsConfig;
-import antiqueatlasautomarker.config.folders.DoomlikeConfig;
-import antiqueatlasautomarker.config.folders.Dungeons2Config;
-import antiqueatlasautomarker.config.folders.EnchantmentConfig;
-import antiqueatlasautomarker.config.folders.IceAndFireConfig;
-import antiqueatlasautomarker.config.folders.InternalConfig;
-import antiqueatlasautomarker.config.folders.LocalisationConfig;
-import antiqueatlasautomarker.config.folders.LycanitesConfig;
-import antiqueatlasautomarker.config.folders.OTGConfig;
-import antiqueatlasautomarker.config.folders.QuarkConfig;
-import antiqueatlasautomarker.config.folders.RoguelikeConfig;
-import antiqueatlasautomarker.config.folders.RuinsConfig;
-import antiqueatlasautomarker.config.folders.StructuresConfig;
-import antiqueatlasautomarker.config.folders.WaystonesConfig;
+import antiqueatlasautomarker.config.folders.*;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.event.ConfigChangedEvent;
--- src/main/java/antiqueatlasautomarker/config/folders/BiomeTileConfig.java
+++ src/main/java/antiqueatlasautomarker/config/folders/BiomeTileConfig.java
@@ -13,7 +13,6 @@ import net.minecraftforge.common.config.Config;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 @MixinConfig(name = AntiqueAtlasAutoMarker.MODID)
